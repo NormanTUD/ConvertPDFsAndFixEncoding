d5aa523aff6c64474b948a0642240e34757d5232
Norman Koch <norman.koch@tu-dresden.de>
2020-11-08 11:53:14 +0100

    added warnings

--- pdftosemiformatted.sh
+++ pdftosemiformatted.sh
@@ -8,12 +8,7 @@ if ! [[ -d $TMPDIR ]]; then
 	mkdir -p $TMPDIR
 fi
 
-function die {
-	red_text $1
-	exit
-}
-
-function dialog {
+function msg {
 	MSG=$1
 	whiptail --title "Message" --msgbox "$MSG" 8 78
 }
@@ -22,17 +17,21 @@ function red_text {
 	echo -e "\e[101m$1\e[0m"
 }
 
+function die {
+	red_text $1
+	exit
+}
+
 function convert_to_text {
 	for FILENAME in $@; do
-		set -x
 		pdftotext -layout -nopgbrk -enc UTF-8 $FILENAME - > $FILENAME.txt
 		FILENAME_WITHOUT_EXTENSION=$(echo "$FILENAME" | sed -e 's/\.pdf$//')
 
-		cat $FILENAME.txt | uconv -f utf8 -t utf8 -x Any-NFKC > ${FILENAME}_corrected.txt
+		TMPFILE=${FILENAME}_corrected.txt
+		cat $FILENAME.txt | uconv -f utf8 -t utf8 -x Any-NFKC > $TMPFILE
 		mv "$FILENAME.txt" $TMPDIR
 
-		mv "${FILENAME}_corrected.txt" "$FILENAME_WITHOUT_EXTENSION.txt"
-		set +x
+		mv "$TMPFILE" "$FILENAME_WITHOUT_EXTENSION.txt"
 	done
 }
 
@@ -57,7 +56,7 @@ function main {
 	$str = "";
 while (my $filename = <*.pdf>) {
 	if($filename =~ m#\s#) {
-		warn qq#Die Datei "$filename" beinhaltet ein Leerzeichen. Leerzeichen machen Probleme. Daher kann ich sie nicht bearbeiten.\n#;
+		warn qq#Die Datei "$filename" beinhaltet ein Leerzeichen. Leerzeichen machen Probleme. Daher kann ich sie nicht bearbeiten. Benenne die Datei um.\n#;
 	} else {
 		$filename_without_extension = $filename;
 		$filename_without_extension =~ s#\.pdf$##g;
@@ -65,7 +64,7 @@ while (my $filename = <*.pdf>) {
 		if(-e "$filename_without_extension.txt") {
 			$on = "OFF";
 		}
-		$str .= qq#"$filename" "$filename" $on #;
+		$str .= qq#"$filename" "$filename_without_extension.txt" $on #;
 	}
 }
 print $str
@@ -79,7 +78,7 @@ print $str
 
 		eval "convert_to_text $(echo $FILESTOCONVERT)"
 	else
-		dialog "There are no *.pdf-files in the current folder ($(pwd))"
+		msg "There are no *.pdf-files in the current folder ($(pwd))"
 	fi
 }
 
